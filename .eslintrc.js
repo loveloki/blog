766c7570e4cb6762532a95620a588a1a56b89f6e
Xleine <919143384@qq.com>
2021-06-24 16:04:41 +0800

    update 移动文件目录，清除不用的代码和文件

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,7 @@ module.exports = {
   extends: ['alloy', 'alloy/react'],
   env: {
     browser: true,
+    jest: true,
   },
   globals: {},
   rules: {},
---
+++ src/components/App/index.js
@@ -0,0 +1,11 @@
+import './index.css'
+
+function App() {
+  return (
+    <div className="h-screen grid place-content-center">
+      <span className="text-3xl hover:text-green-200"> Xleine&lsquo;s Blog</span>
+    </div>
+  )
+}
+
+export default App
