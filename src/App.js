468eac691397cd64928ac49e5d8db23766112dd0
Xleine <919143384@qq.com>
2021-06-24 14:20:31 +0800

    add 添加 tailwind

--- src/App.js
+++ src/App.js
@@ -3,13 +3,13 @@ import './App.css'
 
 function App() {
   return (
-    <div className="App">
+    <div className="App bg-blue-500">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
+        <a className="text-blue-200" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
           Learn React
         </a>
       </header>
