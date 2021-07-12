6101310bfb3bce9bc0d3df765e00dc704bde6853
Xleine <919143384@qq.com>
2021-07-12 21:36:48 +0800

    add 添加 eslint-plugin-react-hooks 插件

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,9 +1,13 @@
 module.exports = {
   extends: ['alloy', 'alloy/react'],
+  plugins: ['react-hooks'],
   env: {
     browser: true,
     jest: true,
   },
   globals: {},
-  rules: {},
+  rules: {
+    'react-hooks/rules-of-hooks': 'error',
+    'react-hooks/exhaustive-deps': 'warn',
+  },
 }
