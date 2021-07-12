ec96701183fa83672526832b13d4dcfd4620c899
Xleine <919143384@qq.com>
2021-07-12 23:55:45 +0800

    add 使用 catalog hooks 和 document hooks 完成列表和文章页

--- src/components/Article/index.js
+++ src/components/Article/index.js
@@ -1,18 +1,24 @@
 import { useEffect, useState } from 'react'
 import ReactMarkdown from 'react-markdown'
 import { useLocation } from 'react-router-dom'
+import useCatalog from '../../hooks/catalog'
+import useDocumentTitle from '../../hooks/document-title'
 import './index.css'
 
 function Article() {
   const [markdownContent, setMarkdownContent] = useState(null)
   const location = useLocation()
   const [, , year, month, name] = location.pathname.split('/')
+  const catalogArray = useCatalog()
+
+  const title = catalogArray.find((item) => item.name === name)?.title
+  useDocumentTitle(title || '文章加载中...')
 
   useEffect(() => {
     fetch(`${process.env.PUBLIC_URL}/article/${year}/${month}/${name}.md`)
       .then((data) => data.text())
       .then((data) => setMarkdownContent(data))
-  }, [])
+  }, [year, month, name])
 
   return <ReactMarkdown className="markdown-body">{markdownContent}</ReactMarkdown>
 }
--- src/components/List/index.js
+++ src/components/List/index.js
@@ -1,20 +1,14 @@
-import { useEffect, useState } from 'react'
 import { Link } from 'react-router-dom'
+import useCatalog from '../../hooks/catalog'
 import './index.css'
 
 function List() {
-  const [list, setList] = useState([])
-
-  useEffect(() => {
-    fetch(`${process.env.PUBLIC_URL}/catalog.json`)
-      .then((data) => data.json())
-      .then((arr) => setList(arr))
-  }, [])
+  const catalogArray = useCatalog()
 
   return (
     <article className="max-w-2xl m-auto">
       <ul className="flex flex-col items-center">
-        {list.map(({ title, name, description, year, month }) => (
+        {catalogArray.map(({ title, name, description, year, month }) => (
           <li className="flex w-full h-32 mt-2" key={name}>
             <article className="flex-1 pl-1 pt-1">
               <Link to={`article/${year}/${month}/${name}`}>
