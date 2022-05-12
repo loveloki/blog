90232af7160f642a26b725d6db5c347cb97d2aa6
Xleine <919143384@qq.com>
2022-05-12 08:21:23 +0800

    refactor: 修改结构

---
+++ src/Home/Home.js
@@ -0,0 +1,12 @@
+import './Home.css'
+
+function Home() {
+  return (
+    <main>
+      <h1>Hello Blog</h1>
+      <code>let author = &quot;Xleine&quot;</code>
+    </main>
+  )
+}
+
+export default Home
