0d0a977cafcdd13a2fbb7a8ea9396515bdcbba3a
xleine <xleine@qq.com>
2022-07-06 00:46:55 +0800

    feat: 修改样式

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,7 +1,12 @@
+import List from './List/List'
 import './index.css'
 
 function App() {
-  return <div className="App">Xleine&#x27;s Blog</div>
+  return (
+    <div className="App">
+      <article>Xleine&#x27;s Blog</article>
+    </div>
+  )
 }
 
 export default App
