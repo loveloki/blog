dee82853d5d9fa1d240707f6a6ba85012a7a8569
xleine <xleine@qq.com>
2022-07-12 01:30:32 +0800

    feat: 加载完字体文件再渲染首页、修改样式

--- src/App/index.tsx
+++ src/App/index.tsx
@@ -1,13 +1,25 @@
+import useFontsReady from '@hooks/useFontsReady'
 import List from './List/List'
 import './index.css'
 
 function App() {
+  const isFontsReady = useFontsReady()
+
   return (
     <main className="App">
-      <aside>
-        <List />
-      </aside>
-      <article>Xleine&#x27;s Blog</article>
+      {isFontsReady ? (
+        <>
+          <aside>
+            <List />
+          </aside>
+          <article>Xleine&#x27;s Blog</article>
+        </>
+      ) : (
+        <div className="font-loading">
+          加载字体中
+          <x-loading-dot />
+        </div>
+      )}
     </main>
   )
 }
--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
+import '@/web-components/x-loading-dot'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
