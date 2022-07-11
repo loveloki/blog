03472bc6b87a022f5f69f3adbf22c0ce249486c4
xleine <xleine@qq.com>
2022-07-11 16:12:13 +0800

    refactor: 调整配置，修复 key 重复 bug

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
 import { defineConfig } from 'vite'
 import eslintPlugin from '@nabla/vite-plugin-eslint'
 import react from '@vitejs/plugin-react'
-import ViteRestart from 'vite-plugin-restart'
+import { resolve } from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    eslintPlugin(),
-    ViteRestart({
-      restart: ['vite.config.ts'],
-    }),
-  ],
+  plugins: [react(), eslintPlugin()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+      '@hooks': resolve(__dirname, './src/hooks'),
+    },
+  },
 })
