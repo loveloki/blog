9d700477e54d5ef880cbbb7ff7da2717ec005278
xleine <xleine@qq.com>
2022-07-26 01:06:54 +0800

    feat: 根据url，渲染相应文件内容

--- src/App/MarkDown/MarkDown.tsx
+++ src/App/MarkDown/MarkDown.tsx
@@ -1,13 +1,24 @@
-import { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import { useRemark } from 'react-remark'
-import mk from '@/article/test.md?raw'
+import { useParams } from 'react-router-dom'
+import articles from '@/article'
 
 const ExampleComponent = () => {
+  const [text, setText] = useState('')
   const [reactContent, setMarkdownSource] = useRemark()
+  const { id } = useParams()
 
   useEffect(() => {
-    setMarkdownSource(mk)
-  }, [])
+    if (articles.has(id)) {
+      setText(articles.get(id))
+    } else {
+      setText('404!')
+    }
+  }, [id])
+
+  useEffect(() => {
+    setMarkdownSource(text)
+  }, [text])
 
   return reactContent
 }
---
+++ src/article/test2.md
@@ -0,0 +1,3 @@
+# test2.md
+
+**this is test2 file**
