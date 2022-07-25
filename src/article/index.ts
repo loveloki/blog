29af0693f9752ecbcedef5ff681fc4f2c6c670fd
xleine <xleine@qq.com>
2022-07-26 00:57:33 +0800

    feat: 添加全局 import，导入所有的 markdown 文件

---
+++ src/article/index.ts
@@ -0,0 +1,14 @@
+const modules = import.meta.glob('./**/*.md', { as: 'raw' })
+const articles = new Map()
+
+for (const path in modules) {
+  if (modules.hasOwnProperty(path)) {
+    modules[path]().then((mod) => {
+      const filename = path.split('/').pop()?.slice(0, -3)
+
+      articles.set(filename, mod)
+    })
+  }
+}
+
+export default articles
