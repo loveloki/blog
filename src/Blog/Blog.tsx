dae2b5d7ce06bc2dc2ffdc18803b477eafbc487e
xleine <xleine@qq.com>
2023-03-12 19:59:31 +0800

    refactor: 调整结构

---
+++ src/Blog/Blog.tsx
@@ -0,0 +1,13 @@
+import './Blog.css'
+
+function App() {
+  return (
+    <div id="Blog">
+      <header>a simple blog</header>
+      <main>文章列表</main>
+      <aside>其他信息，放在侧边 比如阅读书籍，个人信息</aside>
+    </div>
+  )
+}
+
+export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App'
+import Blog from './Blog'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <Blog />
   </React.StrictMode>,
 )
