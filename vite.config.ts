ba8f299d2dbe960d971fb8163ffdc225f49338ff
xleine <xleine@qq.com>
2023-03-12 19:37:53 +0800

    init

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from 'vite'
+import eslintPlugin from '@nabla/vite-plugin-eslint'
+import react from '@vitejs/plugin-react-swc'
+import { resolve } from 'path'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), eslintPlugin()],
+  resolve: {
+    alias: {
+      '@': resolve(__dirname, './src'),
+    },
+  },
+})
