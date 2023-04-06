9d5912fe83768ccd6fc71b4cdbbe6b2374049445
xleine <xleine@qq.com>
2023-04-06 23:28:05 +0800

    feat: 修改usePosts，添加catalog

--- src/hooks/usePosts.ts
+++ src/hooks/usePosts.ts
@@ -1,10 +1,27 @@
 import { useEffect, useState } from 'react'
 
+interface Post {
+  name: String
+  desc: String
+  tags: Array<String>
+}
+
+const path = '/catalog.json'
+
 const usePosts = () => {
-  const [posts, setPosts] = useState<{ name: string }[]>([])
+  const [posts, setPosts] = useState<Post[]>([])
 
   useEffect(() => {
-    setPosts([{ name: 'test' }])
+    fetch(path)
+      .then((res) => res.json())
+      .then((json) => {
+        console.log({ json })
+        setPosts(json)
+      })
+      .catch((e) => {
+        console.warn(e)
+        setPosts([])
+      })
   }, [])
 
   return posts
