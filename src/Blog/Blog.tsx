c978ac02062a816b4cff617518793bd2215411af
xleine <xleine@qq.com>
2023-04-11 00:10:24 +0800

    feat: modify title

--- src/Blog/Blog.tsx
+++ src/Blog/Blog.tsx
@@ -1,4 +1,4 @@
-import { Link, Outlet } from 'react-router-dom'
+import { Link, Outlet, useParams } from 'react-router-dom'
 import usePosts from '../hooks/usePosts'
 import useIsHome from '../hooks/useIsHome'
 import './Blog.css'
@@ -6,13 +6,25 @@ import './Blog.css'
 function App() {
   return (
     <div id="Blog">
-      <header>a simple blog</header>
+      <Header />
       <main>
         <Content />
       </main>
       <aside>其他信息，放在侧边 比如阅读书籍，个人信息</aside>
     </div>
   )
+}
+
+function Header() {
+  const isHome = useIsHome()
+  const posts = usePosts()
+  const param = useParams()
+
+  const title = isHome
+    ? 'a simple blog'
+    : posts.find(({ id }) => id === param.id)?.title
+
+  return <header>{title}</header>
 }
 
 function Content() {
